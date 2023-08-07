a902c2bca960cf43f9bbac42d564ddb8c68d7c13
Kelsey Nagy <nagy8907@gmail.com>
2023-08-06 20:06:53 -0400

    #U5_04b Completed Delete Message

--- Server/controllers/message.controller.js
+++ Server/controllers/message.controller.js
@@ -16,7 +16,7 @@ const errorResponse = (res, error) => {
     )
 }
 
-//TODO POST - create new message
+//TODO POST - create new message ($push)
 router.post('/', validateSession, async (req, res) => {
     try {
         
@@ -25,21 +25,35 @@ router.post('/', validateSession, async (req, res) => {
 
         //2. Create a new object using the Model
         const message = new Message({
-            date, text, owner, room,
+            date: new Date,
+            text,
+            owner,
+            room,
             owner_id: req.user.id
         });
 
         //3. Use mongoose method to save to MongoDB (storing it)
-        const newMessage = await message.save();
+        
+        await message.save();
 
          // Update the 'messages' array of the corresponding room
          const roomToUpdate = await Room.findById(room);
          if (!roomToUpdate) {
              return res.status(404).json({ error: "Room not found." });
          }
- 
-         roomToUpdate.messages.push(newMessage);
-         await roomToUpdate.save();
+        
+        
+
+         
+         await Room.findOneAndUpdate(
+            {_id: roomToUpdate},
+            {$push: { messages: message} }
+         )
+
+     
+
+
+
 
         //4. Client response
         res.status(200).json({
@@ -52,6 +66,8 @@ router.post('/', validateSession, async (req, res) => {
         errorResponse(res, err);
     }
 });
+
+
 
 
 // TODO GET All Messages
@@ -68,7 +84,7 @@ router.get('/:roomId', async (req, res) => {
 
         // Find all messages from the specified room
         const messages = await Message.find({ room: roomId });
-        
+
         if (messages.length > 0) {
             res.status(200).json({
                 messages: messages,
@@ -87,6 +103,87 @@ router.get('/:roomId', async (req, res) => {
 
 
 
+//TODO PATCH One - Make Updates (findOneAndUpdate)
+router.patch('/:id', validateSession, async (req, res) => {
+    try {
+        
+        //1. Pull value from parameter
+        const { id } = req.params;
+
+        const filter = {_id: id, owner_id: req.user._id}
+
+        //2. Pull data from the body
+        const info = req.body;
+        
+
+        //3. Use method to locate document based off ID and pass in new info.
+        const returnOption = {new: true};
+
+       
+        const updated = await Message.findOneAndUpdate(filter, info, returnOption);
+        if(!updated) throw new Error('Message not owned by user')
+
+        //4. Respond to client.
+        res.status(200).json({
+            message: `${updated.title} Updated!`,
+            updated
+        })
+
+    } catch (err) {
+        errorResponse(res, err)
+    }
+});
+
+
+
+//TODO DELETE One - delete ($pull) in both places 
+
+router.delete('/:id/:roomId', validateSession, async (req, res) => {
+    try {
+        //1. Capture ID
+        const { id } = req.params;
+        const { roomId } = req.params;
+        
+
+        //2. Use delete method to locate and remove based off ID
+        const roomToUpdate = await Room.findById(id);
+        const deleteMessage = await Message.deleteOne({_id: id, owner: req.user._id});
+
+        const updatedMessages = await Message.find({ room: roomId });
+        const returnOption = { new: true }
+
+        const updatedRoom = await Room.findByIdAndUpdate(
+            { _id: roomId }, // Find the room where the user is the owner
+            
+            { messages: updatedMessages },
+            returnOption
+        );
+
+            
+
+        
+
+        //3. Respond to client.
+        deleteMessage.deletedCount === 1 ?
+            updatedRoom ? 
+                res.status(200).json({
+                    message: `Message has been removed, Room collection updated.`
+                }) :
+                res.status(404).json({
+                    message: `Could not find room to update`
+                })
+            :
+            res.status(404).json({
+                message: `No message in collection, could not delete.`
+            })
+
+
+    } catch (err) {
+        errorResponse(res, err)
+        
+    }
+});
+
 
 
 
@@ -94,3 +191,4 @@ router.get('/:roomId', async (req, res) => {
 
 
 module.exports = router;
+
